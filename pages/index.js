61e1f7744ee15ff05596bca2b7cbe238ccb5f4de
Andre Parsons <andrparsons@gmail.com>
2021-01-21 12:41:07 -0500

    mobile styling for partners section

--- pages/index.js
+++ pages/index.js
@@ -79,18 +79,20 @@ export default function Home() {
               <Google />
               <Nvidia />
             </div>
-            <h2 className={styles.partnerTitle}>Who we work with</h2>
-            <p className={styles.darkText}>
-              Today, millions of people around the world have successfully
-              connected their accounts to apps they love using our API. We
-              provide developers with the tools they need to create easy and
-              accessible experiences for their users.
-            </p>
-            <Link href="/about">
-              <a>
-                <Button btnStyle="secondary">About Us</Button>
-              </a>
-            </Link>
+            <div className={styles.partnerContent}>
+              <h2 className={styles.partnerTitle}>Who we work with</h2>
+              <p className={`${styles.darkText} ${styles.partnerText}`}>
+                Today, millions of people around the world have successfully
+                connected their accounts to apps they love using our API. We
+                provide developers with the tools they need to create easy and
+                accessible experiences for their users.
+              </p>
+              <Link href="/about">
+                <a>
+                  <Button btnStyle="secondary">About Us</Button>
+                </a>
+              </Link>
+            </div>
           </Container>
         </div>
 
